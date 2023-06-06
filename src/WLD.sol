a301a937b1dd81b50d4c6e96fba22e4f2e524bff
dcbuilder.eth <dcbuilder@protonmail.com>
2023-06-06 17:26:13 +0200

    add(docs, tests), update(errors, modifiers)

--- src/WLD.sol
+++ src/WLD.sol
@@ -1,30 +1,84 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 import {Ownable2Step} from "openzeppelin/access/Ownable2Step.sol";
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 
-
+/// @title WLD token
+/// @author Worldcoin
+/// @notice Contract for Worldcoin's ERC20 WLD token
 contract WLD is ERC20, Ownable2Step {
+    ///////////////////////////////////////////////////////////////////
+    ///                           STORAGE                           ///
+    ///////////////////////////////////////////////////////////////////
+
+    /// @notice Information about the supply of the WLD token at a given point in time
     struct SupplyInfo {
         uint256 timestamp;
         uint256 amount;
     }
 
+    /// @notice The address of the minter
+    address public minter;
+
+    /// @notice Inflation variables, formula in _mint @dev description
     uint256 private _inflationCapPeriod;
     uint256 private _inflationCapNumerator;
     uint256 private _inflationCapDenominator;
-    uint256 private _mintLockInPeriod;
     uint256 private _inflationPeriodCursor;
-    string private _symbol;
-    string private _name;
-    uint8 private _decimals;
-    address public minter;
-    SupplyInfo[] private _supplyHistory;
 
-    // ********************
-    // *** Construction ***
-    // ********************
+    /// @notice How many seconds until the mint lock-in period is over
+    uint256 private _mintLockInPeriod;
+
+    /// @notice The symbol of the token
+    string private _symbol;
+
+    /// @notice The name of the token
+    string private _name;
+
+    /// @notice The number of decimals for the WLD token
+    uint8 private _decimals;
+
+    /// @notice The history of the WLD token's supply
+    SupplyInfo[] public supplyHistory;
+
+    /// @notice Emitted in revert if the mint lock-in period is not over.
+    error MintLockInPeriodNotOver();
+
+    /// @notice Emmitted in revert if the caller is not the minter.
+    error NotMinter();
+
+    /// @notice Emmitted in revert if the inflation cap has been reached.
+    error InflationCapReached();
+
+    /// @notice Emmitted in revert if the owner attempts to resign ownership.
+    error CannotRenounceOwnership();
+
+    ///////////////////////////////////////////////////////////////////
+    ///                          MODIFIERS                          ///
+    ///////////////////////////////////////////////////////////////////
+
+    /// @notice Requires that the current time is after the mint lock-in period.
+    /// @custom:revert MintLockInPeriodNotOver The mint lock-in period is not over.
+    modifier onlyPostMintLockInPeriod() {
+        if (block.timestamp < _getConstructionTime() + _mintLockInPeriod) {
+            revert MintLockInPeriodNotOver();
+        }
+
+        _;
+    }
+
+    /// @notice Requires that the caller is the minter.
+    /// @custom:revert NotMinter The caller is not the minter.
+    modifier onlyMinter() {
+        if (_msgSender() != minter) revert NotMinter();
+        _;
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ///                         CONSTRUCTOR                         ///
+    ///////////////////////////////////////////////////////////////////
 
     constructor(
         string memory name_,
@@ -49,52 +103,66 @@ contract WLD is ERC20, Ownable2Step {
         for (uint256 i = 0; i < initialHolders.length; i++) {
             _update(address(0), initialHolders[i], initialAmounts[i]);
         }
-        _supplyHistory.push(SupplyInfo(block.timestamp, totalSupply()));
+        supplyHistory.push(SupplyInfo(block.timestamp, totalSupply()));
     }
 
-    // ****************
-    // *** Metadata ***
-    // ****************
+    ///////////////////////////////////////////////////////////////////
+    ///                           METADATA                          ///
+    ///////////////////////////////////////////////////////////////////
 
+    /// @notice Returns the name of the token
     function name() public view override returns (string memory) {
         return _name;
     }
 
+    /// @notice Returns that symbol of the token
     function symbol() public view override returns (string memory) {
         return _symbol;
     }
 
+    /// @notice Returns the number of decimals for the token
     function decimals() public view override returns (uint8) {
         return _decimals;
     }
 
-    // *********************
-    // *** Admin actions ***
-    // *********************
+    ///////////////////////////////////////////////////////////////////
+    ///                        ADMIN ACTIONS                        ///
+    ///////////////////////////////////////////////////////////////////
 
+    /// @notice Sets the name of the token
+    /// @param name_ new name
     function setName(string memory name_) public onlyOwner {
         _name = name_;
     }
 
+    /// @notice Sets the token symbol
+    /// @param symbol_ new symbol
     function setSymbol(string memory symbol_) public onlyOwner {
         _symbol = symbol_;
     }
 
+    /// @notice Sets the token decimals
+    /// @param decimals_ new decimals
     function setDecimals(uint8 decimals_) public onlyOwner {
         _decimals = decimals_;
     }
 
-    function renounceOwnership() public view override onlyOwner {
-        revert("Cannot renounce ownership");
-    }
-
+    /// @notice Updates minter
+    /// @dev onlyOwner
+    /// @param minter_ new Minter address
     function setMinter(address minter_) public onlyOwner {
         minter = minter_;
     }
 
-    // **********************
-    // *** Minter Actions ***
-    // **********************
+    /// @notice Prevents the owner from renouncing ownership
+    /// @dev onlyOwner
+    function renounceOwnership() public view override onlyOwner {
+        revert("Cannot renounce ownership");
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ///                        MINTER ACTIONS                       ///
+    ///////////////////////////////////////////////////////////////////
 
     /// @notice Mints new tokens and assigns them to the target address.
     /// @dev This function performs inflation checks. Their semantics is as follows:
@@ -103,28 +171,25 @@ contract WLD is ERC20, Ownable2Step {
     ///       making sure that the total supply of tokens does not increase by more than
     ///       `1 + (inflationCapNumerator / inflationCapDenominator)` times during any
     ///       `_inflationCapPeriod` seconds period.
-    function mint(address to, uint256 amount) public {
-        _requireMinter();
-        _requirePostMintLockInPeriod();
+    function mint(address to, uint256 amount) public onlyMinter onlyPostMintLockInPeriod {
         _advanceInflationPeriodCursor();
         uint256 oldTotal = _getTotalSupplyInflationPeriodAgo();
         uint256 newTotal = totalSupply() + amount;
         _requireInflationCap(oldTotal, newTotal);
-        _supplyHistory.push(SupplyInfo(block.timestamp, newTotal));
+        supplyHistory.push(SupplyInfo(block.timestamp, newTotal));
         _mint(to, amount);
     }
 
-    function _requireInflationCap(
-        uint256 oldTotal,
-        uint256 newTotal
-    ) internal view {
-        require(
-            newTotal * _inflationCapDenominator <=
-                oldTotal * (_inflationCapNumerator + _inflationCapDenominator),
-            "Inflation cap reached"
-        );
+    /// @notice Prevents the minter from minting tokens above the inflation cap.
+    /// @param oldTotal The total supply before the requested mint
+    /// @param newTotal The total supply after the requested mint
+    function _requireInflationCap(uint256 oldTotal, uint256 newTotal) internal view {
+        if (newTotal * _inflationCapDenominator > oldTotal * (_inflationCapNumerator + _inflationCapDenominator)) {
+            revert InflationCapReached();
+        }
     }
 
+    /// @notice Advances the inflation period cursor to the _inflationPeriodCursor by 1.
     function _advanceInflationPeriodCursor() internal {
         uint256 currentTimestamp = block.timestamp;
         uint256 currentPosition = _inflationPeriodCursor;
@@ -135,32 +200,21 @@ contract WLD is ERC20, Ownable2Step {
         // That means that the cursor will point to the youngest timestamp that
         // is older than said period, i.e. the supply at _inflationCapPeriod ago.
         while (
-            currentPosition + 1 < _supplyHistory.length &&
-            _supplyHistory[currentPosition + 1].timestamp +
-                _inflationCapPeriod <
-            currentTimestamp
+            currentPosition + 1 < supplyHistory.length
+                && supplyHistory[currentPosition + 1].timestamp + _inflationCapPeriod < currentTimestamp
         ) {
             currentPosition += 1;
         }
         _inflationPeriodCursor = currentPosition;
     }
 
+    /// @notice Returns the total supply .
     function _getTotalSupplyInflationPeriodAgo() internal view returns (uint256) {
-        return _supplyHistory[_inflationPeriodCursor].amount;
+        return supplyHistory[_inflationPeriodCursor].amount;
     }
 
-    function _requireMinter() internal view {
-        require(_msgSender() == minter, "Caller is not the minter");
-    }
-
+    /// @notice Returns the supply at the time of construction.
     function _getConstructionTime() internal view returns (uint256) {
-        return _supplyHistory[0].timestamp;
-    }
-
-    function _requirePostMintLockInPeriod() internal view {
-        require(
-            block.timestamp >= _getConstructionTime() + _mintLockInPeriod,
-            "Mint lock-in period not over"
-        );
+        return supplyHistory[0].timestamp;
     }
 }
--- test/WLD.t.sol
+++ test/WLD.t.sol
@@ -5,7 +5,10 @@ import "forge-std/Test.sol";
 import "../src/WLD.sol";
 
 contract WLDTest is Test {
-    uint _initialTime = 1234 seconds;
+    ///////////////////////////////////////////////////////////////////
+    ///                           STORAGE                           ///
+    ///////////////////////////////////////////////////////////////////
+    uint256 _initialTime = 1234 seconds;
     string _symbol = "WLD";
     string _name = "Worldcoin";
     uint8 _decimals = 18;
@@ -20,8 +23,23 @@ contract WLDTest is Test {
     WLD _token;
     address _minter = address(uint160(uint256(keccak256("wld minter"))));
 
+    address _owner = address(0x123);
+
+    /// @notice Emitted in revert if the mint lock-in period is not over.
+    error MintLockInPeriodNotOver();
+
+    /// @notice Emmitted in revert if the caller is not the minter.
+    error NotMinter();
+
+    /// @notice Emmitted in revert if the inflation cap has been reached.
+    error InflationCapReached();
+
+    /// @notice Emmitted in revert if the owner attempts to resign ownership.
+    error CannotRenounceOwnership();
+
     function setUp() public {
         vm.warp(_initialTime);
+        vm.startPrank(_owner);
         _token = new WLD(
             _symbol,
             _name,
@@ -36,48 +54,100 @@ contract WLDTest is Test {
         _token.setMinter(_minter);
     }
 
-    modifier asMinter {
+    ///////////////////////////////////////////////////////////////////
+    ///                          MODIFIERS                          ///
+    ///////////////////////////////////////////////////////////////////
+
+    modifier asOwner() {
+        vm.startPrank(_owner);
+        _;
+        vm.stopPrank();
+    }
+
+    modifier asMinter() {
         vm.startPrank(_minter);
         _;
         vm.stopPrank();
     }
 
+    ///////////////////////////////////////////////////////////////////
+    ///                        ADMIN ACTIONS                        ///
+    ///////////////////////////////////////////////////////////////////
+
+    /// @notice Tests that the owner can set a new minter
+    function testSetMinterSucceeds(address minter) public asOwner {
+        _token.setMinter(minter);
+
+        assert(_token.minter() == minter);
+    }
+
+    /// @notice Tests that the owner can set a new name
+    function testSetNameSucceeds(string memory name) public asOwner {
+        _token.setName(name);
+
+        assert(keccak256(bytes(_token.name())) == keccak256(bytes(name)));
+    }
+
+    /// @notice Tests that the owner can set a new symbol
+    function testSetSymbol(string memory symbol) public asOwner {
+        _token.setSymbol(symbol);
+
+        assert(keccak256(bytes(_token.symbol())) == keccak256(bytes(symbol)));
+    }
+
+    /// @notice Tests that the owner can change the decimal precisition of the token (should be 9 or 18).
+    function testSetDecimals(uint8 decimals) public {
+        vm.assume(decimals > 0);
+        _token.setDecimals(decimals);
+
+        assert(_token.decimals() == decimals);
+    }
+
+    /// @notice Tests that the minter can mint tokens after the lock-in period.
     function testMintAccessControl(address minter) public {
+        vm.assume(minter != _minter);
         vm.warp(_initialTime + _mintLockInPeriod + 20 seconds);
+
         vm.startPrank(minter);
-        if (minter != _minter) {
-            vm.expectRevert(bytes("Caller is not the minter"));
-        }
+        vm.expectRevert(NotMinter.selector);
         _token.mint(address(this), 20);
     }
 
-    function testInitialDistributionHappens() public {
+    ///////////////////////////////////////////////////////////////////
+    ///                         DISTRIBUTION                        ///
+    ///////////////////////////////////////////////////////////////////
+
+    /// @notice Tests that the initial distribution happens correctly.
+    function testInitialDistributionHappens() public view {
         assert(_token.balanceOf(address(0x123)) == 500);
         assert(_token.balanceOf(address(0x456)) == 500);
         assert(_token.totalSupply() == 1000);
     }
 
-    function testInitialDistributionRestricted(address receiver) public {
-        if (receiver != address(0x123) && receiver != address(0x456)) {
-            assert(_token.balanceOf(receiver) == 0);
-        }
+    /// @notice Tests that the initial distribution is restricted to the initial holders.
+    function testInitialDistributionRestricted(address receiver) public view {
+        vm.assume(receiver != address(0x123) && receiver != address(0x456));
+
+        assert(_token.balanceOf(receiver) == 0);
     }
 
+    /// @notice Tests that the initial distribution is restricted properly.
     function testMintsLockInPeriod() public asMinter {
-        vm.expectRevert(bytes("Mint lock-in period not over"));
+        vm.expectRevert(MintLockInPeriodNotOver.selector);
         _token.mint(address(this), 100);
         vm.warp(_initialTime + _mintLockInPeriod - 1);
-        vm.expectRevert(bytes("Mint lock-in period not over"));
+        vm.expectRevert(MintLockInPeriodNotOver.selector);
         _token.mint(address(this), 100);
         vm.warp(_initialTime + _mintLockInPeriod);
         _token.mint(address(this), 100);
         assert(_token.balanceOf(address(this)) == 100);
     }
 
+    /// @notice Tests that the inflation cap is enforced.
     function testInflationCap() public asMinter {
         vm.warp(_initialTime + _mintLockInPeriod);
         // fails – more than initial supply + inflation cap
-        vm.expectRevert(bytes("Inflation cap reached"));
+        vm.expectRevert(InflationCapReached.selector);
         _token.mint(address(this), 101);
         // works – below initial supply + inflation
         _token.mint(address(this), 50); // supply == 1050
@@ -85,18 +155,27 @@ contract WLDTest is Test {
         // works - minting up to yearly cap
         _token.mint(address(this), 50); // supply == 1100
         // fails - exceeding yearly cap
-        vm.expectRevert(bytes("Inflation cap reached"));
+        vm.expectRevert(InflationCapReached.selector);
         _token.mint(address(this), 1);
         vm.warp(_initialTime + _mintLockInPeriod + _inflationCapPeriod + 1001 seconds);
         // works - next cap is 110
         _token.mint(address(this), 60); // supply == 1160
         // fails - exceeding yearly cap
-        vm.expectRevert(bytes("Inflation cap reached"));
+        vm.expectRevert(InflationCapReached.selector);
         _token.mint(address(this), 51);
         // succeeds - 50 is still below cap
         _token.mint(address(this), 50); // supply == 1210
 
         assert(_token.balanceOf(address(this)) == 210);
         assert(_token.totalSupply() == 1210);
+    }
+
+    ///////////////////////////////////////////////////////////////////
+    ///                           REVERTS                           ///
+    ///////////////////////////////////////////////////////////////////
+
+    function testRenounceOwnershipReverts() public asOwner {
+        vm.expectRevert("Cannot renounce ownership");
+        _token.renounceOwnership();
     }
 }
