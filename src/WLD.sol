86b7e9dfda17dcda328f2c3d0db3f6cf18ba03e1
dcbuilder.eth <dcbuilder@protonmail.com>
2023-06-06 17:26:51 +0200

    bump(solidity version)

--- src/WLD.sol
+++ src/WLD.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.19;
 
 import {Ownable} from "openzeppelin/access/Ownable.sol";
 import {Ownable2Step} from "openzeppelin/access/Ownable2Step.sol";
