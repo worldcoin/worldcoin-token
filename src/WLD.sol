a2c20fede8134edb1be00eed445d66dc5e975f86
Marcin Kostrzewa <marckostrzewa@gmail.com>
2023-06-21 18:51:11 +0200

    implement the recommended optimizations

--- src/WLD.sol
+++ src/WLD.sol
@@ -17,15 +17,14 @@ contract WLD is ERC20, Ownable2Step {
     address public minter;
 
     /// @notice Inflation variables, formula in _mint @dev description
-    uint256 private _inflationCapPeriod;
-    uint256 private _inflationCapNumerator;
-    uint256 private _inflationCapDenominator;
+    uint256 immutable private _inflationCapPeriod;
+    uint256 immutable private _inflationCapNumerator;
+    uint256 immutable private _inflationCapDenominator;
     uint256 private _currentPeriodEnd;
     uint256 private _currentPeriodInitialSupply;
 
     /// @notice How many seconds until the mint lock-in period is over
-    uint256 private _mintLockInPeriod;
-    uint256 immutable private _deploymentTime;
+    uint256 immutable private _mintUnlockTime;
 
     /// @notice The symbol of the token
     string private _symbol;
@@ -55,7 +54,7 @@ contract WLD is ERC20, Ownable2Step {
         uint256 inflationCapPeriod_,
         uint256 inflationCapNumerator_,
         uint256 inflationCapDenominator_,
-        uint256 mintLockInPeriod_,
+        uint256 mintLockPeriod_,
         address[] memory initialHolders,
         uint256[] memory initialAmounts
     ) ERC20(name_, symbol_) Ownable(msg.sender) {
@@ -64,8 +63,7 @@ contract WLD is ERC20, Ownable2Step {
         _inflationCapPeriod = inflationCapPeriod_;
         _inflationCapNumerator = inflationCapNumerator_;
         _inflationCapDenominator = inflationCapDenominator_;
-        _mintLockInPeriod = mintLockInPeriod_;
-        _deploymentTime = block.timestamp;
+        _mintUnlockTime = mintLockPeriod_ + block.timestamp;
         require(initialAmounts.length == initialHolders.length);
         for (uint256 i = 0; i < initialHolders.length; i++) {
             _update(address(0), initialHolders[i], initialAmounts[i]);
@@ -92,20 +90,20 @@ contract WLD is ERC20, Ownable2Step {
 
     /// @notice Sets the name of the token
     /// @param name_ new name
-    function setName(string memory name_) public onlyOwner {
+    function setName(string memory name_) external onlyOwner {
         _name = name_;
     }
 
     /// @notice Sets the token symbol
     /// @param symbol_ new symbol
-    function setSymbol(string memory symbol_) public onlyOwner {
+    function setSymbol(string memory symbol_) external onlyOwner {
         _symbol = symbol_;
     }
 
     /// @notice Updates minter
     /// @dev onlyOwner
     /// @param minter_ new Minter address
-    function setMinter(address minter_) public onlyOwner {
+    function setMinter(address minter_) external onlyOwner {
         minter = minter_;
     }
 
@@ -139,9 +137,9 @@ contract WLD is ERC20, Ownable2Step {
     ///        such that inflation measured between (t + tc) and (t + tc + inflation period)
     ///        does not exceed the inflation cap. In other words, period over period inflation is
     ///        bounded by the inflation cap at least for some amount of time during each period.
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) external {
         _requireMinter();
-        _requirePostMintLockInPeriod();
+        _requirePostMintLockPeriod();
         _adjustCurrentPeriod();
         _requireInflationCap(amount);
         _mint(to, amount);
@@ -172,8 +170,8 @@ contract WLD is ERC20, Ownable2Step {
 
     /// @notice Requires that the current time is after the mint lock-in period.
     /// @custom:revert MintLockInPeriodNotOver The mint lock-in period is not over.
-    function _requirePostMintLockInPeriod() internal view {
-        if (block.timestamp < _deploymentTime + _mintLockInPeriod) {
+    function _requirePostMintLockPeriod() internal view {
+        if (block.timestamp < _mintUnlockTime) {
             revert MintLockInPeriodNotOver();
         }
     }
