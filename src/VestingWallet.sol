79a9d9e57f4a025f47229df4d0194867266b9aeb
Marcin Kostrzewa <marckostrzewa@gmail.com>
2023-07-07 19:38:24 +0200

    add a vesting wallet

---
+++ src/VestingWallet.sol
@@ -0,0 +1,160 @@
+// SPDX-License-Identifier: UNLICENSED
+// OpenZeppelin Contracts (last updated v4.9.0) (finance/VestingWallet.sol)
+// Modified for Worldcoin
+pragma solidity ^0.8.19;
+
+import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
+import {SafeERC20} from "openzeppelin/token/ERC20/utils/SafeERC20.sol";
+import {Address} from "openzeppelin/utils/Address.sol";
+import {Ownable2Step} from "openzeppelin/access/Ownable2Step.sol";
+import {Ownable} from "openzeppelin/access/Ownable.sol";
+
+
+/**
+ * @title VestingWallet
+ * @dev This contract handles the vesting of Eth and ERC20 tokens for a given beneficiary. Custody of multiple tokens
+ * can be given to this contract, which will release the token to the beneficiary following a given vesting schedule.
+ * The vesting schedule is customizable through the {vestedAmount} function.
+ *
+ * Any token transferred to this contract will follow the vesting schedule as if they were locked from the beginning.
+ * Consequently, if the vesting has already started, any amount of tokens sent to this contract will (at least partly)
+ * be immediately releasable.
+ *
+ * By setting the duration to 0, one can configure this contract to behave like an asset timelock that hold tokens for
+ * a beneficiary until a specified time.
+ *
+ * The beneficiary is controlled through the {Ownable} mechanism, so they can assign the unvested portion of the assets
+ * to another party.
+ */
+contract VestingWallet is Ownable2Step {
+    event EtherReleased(uint256 amount);
+    event ERC20Released(address indexed token, uint256 amount);
+
+    /**
+     * @dev The `beneficiary` is not a valid account.
+     */
+    error VestingWalletInvalidBeneficiary(address beneficiary);
+
+    uint256 private _released;
+    mapping(address => uint256) private _erc20Released;
+    uint64 private immutable _start;
+    uint64 private immutable _duration;
+
+    /**
+     * @dev Set the beneficiary, start timestamp and vesting duration of the vesting wallet.
+     */
+    constructor(address beneficiaryAddress, uint64 startTimestamp, uint64 durationSeconds) payable  Ownable(beneficiaryAddress){
+        if (beneficiaryAddress == address(0)) {
+            revert VestingWalletInvalidBeneficiary(address(0));
+        }
+        _start = startTimestamp;
+        _duration = durationSeconds;
+    }
+
+    /**
+     * @dev The contract should be able to receive Eth.
+     */
+    receive() external payable {}
+
+    /**
+     * @dev Getter for the start timestamp.
+     */
+    function start() public view returns (uint256) {
+        return _start;
+    }
+
+    /**
+     * @dev Getter for the vesting duration.
+     */
+    function duration() public view returns (uint256) {
+        return _duration;
+    }
+
+    /**
+     * @dev Getter for the end timestamp.
+     */
+    function end() public view returns (uint256) {
+        return start() + duration();
+    }
+
+    /**
+     * @dev Amount of eth already released
+     */
+    function released() public view returns (uint256) {
+        return _released;
+    }
+
+    /**
+     * @dev Amount of token already released
+     */
+    function released(address token) public view returns (uint256) {
+        return _erc20Released[token];
+    }
+
+    /**
+     * @dev Getter for the amount of releasable eth.
+     */
+    function releasable() public view returns (uint256) {
+        return vestedAmount(uint64(block.timestamp)) - released();
+    }
+
+    /**
+     * @dev Getter for the amount of releasable `token` tokens. `token` should be the address of an
+     * IERC20 contract.
+     */
+    function releasable(address token) public view returns (uint256) {
+        return vestedAmount(token, uint64(block.timestamp)) - released(token);
+    }
+
+    /**
+     * @dev Release the native token (ether) that have already vested.
+     *
+     * Emits a {EtherReleased} event.
+     */
+    function release() public {
+        uint256 amount = releasable();
+        _released += amount;
+        emit EtherReleased(amount);
+        Address.sendValue(payable(owner()), amount);
+    }
+
+    /**
+     * @dev Release the tokens that have already vested.
+     *
+     * Emits a {ERC20Released} event.
+     */
+    function release(address token) public {
+        uint256 amount = releasable(token);
+        _erc20Released[token] += amount;
+        emit ERC20Released(token, amount);
+        SafeERC20.safeTransfer(IERC20(token), owner(), amount);
+    }
+
+    /**
+     * @dev Calculates the amount of ether that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(uint64 timestamp) public view returns (uint256) {
+        return _vestingSchedule(address(this).balance + released(), timestamp);
+    }
+
+    /**
+     * @dev Calculates the amount of tokens that has already vested. Default implementation is a linear vesting curve.
+     */
+    function vestedAmount(address token, uint64 timestamp) public view returns (uint256) {
+        return _vestingSchedule(IERC20(token).balanceOf(address(this)) + released(token), timestamp);
+    }
+
+    /**
+     * @dev Implementation of the vesting formula. This returns the amount vested, as a function of time, for
+     * an asset given its total historical allocation.
+     */
+    function _vestingSchedule(uint256 totalAllocation, uint64 timestamp) internal view returns (uint256) {
+        if (timestamp < start()) {
+            return 0;
+        } else if (timestamp > end()) {
+            return totalAllocation;
+        } else {
+            return (totalAllocation * (timestamp - start())) / duration();
+        }
+    }
+}
---
+++ test/VestingWallet.t.sol
@@ -0,0 +1,165 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "forge-std/Test.sol";
+import "../src/VestingWallet.sol";
+import "openzeppelin/utils/math/Math.sol";
+import "../src/WLD.sol";
+
+contract VestingWalletTest is Test {
+    uint64 private _duration = 4 * 365 days;
+    uint64 private _start = uint64(block.timestamp) + 1 hours;
+    VestingWallet _wallet;
+    address _beneficiary = address(uint160(uint256(keccak256("beneficiary"))));
+
+    function setUp() public {
+        _wallet = new VestingWallet(_beneficiary, _start, _duration);
+    }
+
+    function testZeroBeneficiary() public {
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                VestingWallet.VestingWalletInvalidBeneficiary.selector,
+                address(0)
+            )
+        );
+        new VestingWallet(address(0), _start, _duration);
+    }
+
+    function testParameterGetters() public {
+        assertEq(_wallet.owner(), _beneficiary);
+        assertEq(_wallet.start(), _start);
+        assertEq(_wallet.duration(), _duration);
+        assertEq(_wallet.end(), _start + _duration);
+    }
+
+    struct ScheduleItem {
+        uint64 timestamp;
+        uint256 expectedAmount;
+    }
+
+    /**
+     * @dev Builds a schedule of `length` points, spaced uniformly between `start` and
+     * `start + duration`.
+     */
+    function buildSchedule(
+        uint256 fullAmount, uint256 length
+    ) public view returns (ScheduleItem[] memory) {
+        ScheduleItem[] memory schedule = new ScheduleItem[](length);
+        for (uint256 i = 0; i < schedule.length; i++) {
+            uint64 timestamp = uint64(_start + (i * _duration) / (length - 1));
+            schedule[i].timestamp = timestamp;
+            schedule[i].expectedAmount = Math.min(
+                fullAmount,
+                (fullAmount * (timestamp - _start)) / _duration
+            );
+        }
+        return schedule;
+    }
+
+    function testEthVestingGetters() public {
+        uint256 amount = 1000;
+        payable(_wallet).transfer(amount);
+        ScheduleItem[] memory schedule = buildSchedule(amount, 64);
+        for (uint256 i = 0; i < schedule.length; i++) {
+            vm.warp(schedule[i].timestamp);
+            assert(
+                _wallet.vestedAmount(schedule[i].timestamp) ==
+                    schedule[i].expectedAmount
+            );
+        }
+    }
+
+    function testEthVestingExecution() public {
+        uint256 amount = 1000;
+        payable(_wallet).transfer(amount);
+        ScheduleItem[] memory schedule = buildSchedule(amount, 64);
+        _wallet.release();
+        assertEq(_beneficiary.balance, 0);
+        for (uint256 i = 0; i < schedule.length; i++) {
+            vm.warp(schedule[i].timestamp);
+            _wallet.release();
+            assertEq(_beneficiary.balance, schedule[i].expectedAmount);
+        }
+    }
+
+    function testERC20VestingGetters() public {
+        uint256 amount = 10000;
+        address[] memory recv = new address[](1);
+        recv[0] = address(_wallet);
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = amount;
+        address token = address(new WLD("Test", "TST", 1, 0, 1, 0, recv, amounts));
+        ScheduleItem[] memory schedule = buildSchedule(amount, 64);
+        for (uint256 i = 0; i < schedule.length; i++) {
+            vm.warp(schedule[i].timestamp);
+            assert(
+                _wallet.vestedAmount(token, schedule[i].timestamp) ==
+                    schedule[i].expectedAmount
+            );
+        }
+    }
+
+    function testERC20VestingExecution() public {
+        uint256 amount = 10000;
+        address[] memory recv = new address[](1);
+        recv[0] = address(_wallet);
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = amount;
+        WLD token = new WLD("Test", "TST", 1, 0, 1, 0, recv, amounts);
+        ScheduleItem[] memory schedule = buildSchedule(amount, 64);
+        _wallet.release(address(token));
+        assertEq(token.balanceOf(_beneficiary), 0);
+        for (uint256 i = 0; i < schedule.length; i++) {
+            vm.warp(schedule[i].timestamp);
+            _wallet.release(address(token));
+            assertEq(token.balanceOf(_beneficiary), schedule[i].expectedAmount);
+        }
+    }
+
+    function testEthOwnershipTransfer() public {
+        uint256 amount = 1000;
+        payable(_wallet).transfer(amount);
+        ScheduleItem[] memory schedule = buildSchedule(amount, 3);
+        address newOwner = address(uint160(uint256(keccak256("new beneficiary"))));
+        vm.warp(schedule[1].timestamp);
+        _wallet.release();
+        assertEq(_beneficiary.balance, 500);
+        assertEq(newOwner.balance, 0);
+        vm.prank(_beneficiary);
+        _wallet.transferOwnership(newOwner);
+        assertEq(_wallet.owner(), _beneficiary);
+        vm.prank(newOwner);
+        _wallet.acceptOwnership();
+        assertEq(_wallet.owner(), newOwner);
+        vm.warp(schedule[2].timestamp);
+        _wallet.release();
+        assertEq(_beneficiary.balance, 500);
+        assertEq(newOwner.balance, 500);
+    }
+
+    function testERC20OwnershipTransfer() public {
+        uint256 amount = 10000;
+        address[] memory recv = new address[](1);
+        recv[0] = address(_wallet);
+        uint256[] memory amounts = new uint256[](1);
+        amounts[0] = amount;
+        WLD token = new WLD("Test", "TST", 1, 0, 1, 0, recv, amounts);
+        ScheduleItem[] memory schedule = buildSchedule(amount, 3);
+        address newOwner = address(uint160(uint256(keccak256("new beneficiary"))));
+        vm.warp(schedule[1].timestamp);
+        _wallet.release(address(token));
+        assertEq(token.balanceOf(_beneficiary), 5000);
+        assertEq(token.balanceOf(newOwner), 0);
+        vm.prank(_beneficiary);
+        _wallet.transferOwnership(newOwner);
+        assertEq(_wallet.owner(), _beneficiary);
+        vm.prank(newOwner);
+        _wallet.acceptOwnership();
+        assertEq(_wallet.owner(), newOwner);
+        vm.warp(schedule[2].timestamp);
+        _wallet.release(address(token));
+        assertEq(token.balanceOf(_beneficiary), 5000);
+        assertEq(token.balanceOf(newOwner), 5000);
+    }
+}
