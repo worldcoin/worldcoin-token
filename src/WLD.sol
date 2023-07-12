023f6f07b7bffdfa00e44bb2f13c7ac32eb125f6
Marcin Kostrzewa <marckostrzewa@gmail.com>
2023-07-12 16:21:30 +0200

    remove admin actions altogether

--- src/WLD.sol
+++ src/WLD.sol
@@ -1,27 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import {Ownable} from "openzeppelin/access/Ownable.sol";
-import {Ownable2Step} from "openzeppelin/access/Ownable2Step.sol";
 import {ERC20} from "openzeppelin/token/ERC20/ERC20.sol";
 
 /// @title WLD token
 /// @author Worldcoin
 /// @notice Contract for Worldcoin's ERC20 WLD token
-contract WLD is ERC20, Ownable2Step {
-    ///////////////////////////////////////////////////////////////////
-    ///                           STORAGE                           ///
-    ///////////////////////////////////////////////////////////////////
-
-    /// @notice The symbol of the token
-    string private _symbol;
-
-    /// @notice The name of the token
-    string private _name;
-
-    /// @notice Emmitted in revert if the owner attempts to resign ownership.
-    error CannotRenounceOwnership();
-
+contract WLD is ERC20 {
     /// @notice Emitted when constructing the contract
     event TokenDeployed(
         string name,
@@ -39,9 +24,7 @@ contract WLD is ERC20, Ownable2Step {
         string memory symbol_,
         address[] memory initialHolders,
         uint256[] memory initialAmounts
-    ) ERC20(name_, symbol_) Ownable(msg.sender) {
-        _name = name_;
-        _symbol = symbol_;
+    ) ERC20(name_, symbol_) {
         require(initialAmounts.length == initialHolders.length);
         for (uint256 i = 0; i < initialHolders.length; i++) {
             _update(address(0), initialHolders[i], initialAmounts[i]);
@@ -52,41 +35,5 @@ contract WLD is ERC20, Ownable2Step {
             initialHolders,
             initialAmounts
         );
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ///                           METADATA                          ///
-    ///////////////////////////////////////////////////////////////////
-
-    /// @notice Returns the name of the token
-    function name() public view override returns (string memory) {
-        return _name;
-    }
-
-    /// @notice Returns that symbol of the token
-    function symbol() public view override returns (string memory) {
-        return _symbol;
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ///                        ADMIN ACTIONS                        ///
-    ///////////////////////////////////////////////////////////////////
-
-    /// @notice Sets the name of the token
-    /// @param name_ new name
-    function setName(string memory name_) external onlyOwner {
-        _name = name_;
-    }
-
-    /// @notice Sets the token symbol
-    /// @param symbol_ new symbol
-    function setSymbol(string memory symbol_) external onlyOwner {
-        _symbol = symbol_;
-    }
-
-    /// @notice Prevents the owner from renouncing ownership
-    /// @dev onlyOwner
-    function renounceOwnership() public view override onlyOwner {
-        revert CannotRenounceOwnership();
     }
 }
--- test/WLD.t.sol
+++ test/WLD.t.sol
@@ -8,20 +8,16 @@ contract WLDTest is Test {
     ///////////////////////////////////////////////////////////////////
     ///                           STORAGE                           ///
     ///////////////////////////////////////////////////////////////////
-    uint256 _initialTime = 1234 seconds;
     string _symbol = "WLD";
     string _name = "Worldcoin";
     address[] _initialHolders = [address(0x123), address(0x456)];
     uint256[] _initialAmounts = [500, 500];
     WLD _token;
-    address _owner = address(0x123);
 
     /// @notice Emmitted in revert if the owner attempts to resign ownership.
     error CannotRenounceOwnership();
 
     function setUp() public {
-        vm.warp(_initialTime);
-        vm.startPrank(_owner);
         _token = new WLD(
             _symbol,
             _name,
@@ -30,41 +26,6 @@ contract WLDTest is Test {
         );
         vm.stopPrank();
     }
-
-    ///////////////////////////////////////////////////////////////////
-    ///                          MODIFIERS                          ///
-    ///////////////////////////////////////////////////////////////////
-
-    modifier asOwner() {
-        vm.startPrank(_owner);
-        _;
-        vm.stopPrank();
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ///                        ADMIN ACTIONS                        ///
-    ///////////////////////////////////////////////////////////////////
-
-
-    /// @notice Tests that the owner can set a new name
-    function testSetNameSucceeds(string memory name) public asOwner {
-        _token.setName(name);
-
-        assert(keccak256(bytes(_token.name())) == keccak256(bytes(name)));
-    }
-
-    /// @notice Tests that the owner can set a new symbol
-    function testSetSymbol(string memory symbol) public asOwner {
-        _token.setSymbol(symbol);
-
-        assert(keccak256(bytes(_token.symbol())) == keccak256(bytes(symbol)));
-    }
-
-    /// @notice Tests that the decimals are hardcoded to 18.
-    function testDecimals() public view {
-        assert(_token.decimals() == 18);
-    }
-
 
     ///////////////////////////////////////////////////////////////////
     ///                         DISTRIBUTION                        ///
@@ -82,14 +43,5 @@ contract WLDTest is Test {
         vm.assume(receiver != address(0x123) && receiver != address(0x456));
 
         assert(_token.balanceOf(receiver) == 0);
-    }
-
-    ///////////////////////////////////////////////////////////////////
-    ///                           REVERTS                           ///
-    ///////////////////////////////////////////////////////////////////
-
-    function testRenounceOwnershipReverts() public asOwner {
-        vm.expectRevert(CannotRenounceOwnership.selector);
-        _token.renounceOwnership();
     }
 }
